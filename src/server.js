8b3dbc36e14987228e772b71566223871c409bb2
CodeWracker <dev.rodrigofs@gmail.com>
2022-11-18 14:54:14 -0300

    criei a estrutura das rotas

--- src/server.js
+++ src/server.js
@@ -1,4 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+const routes = require('./routes')
 
-app.listen(3333, () => console.log('Server is running'))
+// configura o .env
+require('dotenv').config()
+
+// configura o CORS
+app.use(cors())
+
+app.use(express.json())
+app.use(routes);
+
+app.listen(3333, () => {
+    console.log('Server is running');
+})
