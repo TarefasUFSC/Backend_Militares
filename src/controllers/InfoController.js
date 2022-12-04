50d123396e8c33028bca2295b2ad9511e6d802cf
pitaemir <emirbraz.d2@gmail.com>
2022-12-04 17:22:18 -0300

    rota de idioma

--- src/controllers/InfoController.js
+++ src/controllers/InfoController.js
@@ -236,20 +236,20 @@ module.exports = {
         return res.json({ idiomas:linguas });
     },
     async addLinguas(req, res){
-        const { list_linguas } = req.body;
+        const { list_idioma } = req.body;
         const { secret_access_tokenL } = req.headers;
 
-        if(list_linguas == undefined || list_linguas.length == 0)
+        if(list_idioma == undefined || list_idioma.length == 0)
         {return res.status(400).json({ msg: 'Lista de Idiomas vazia' });}
 
-        const idioma = await connection('Idioma').select('*').whereIn('nm_idioma', ListSearchLanguage);
-        const { ListSearchLanguage } =  list_linguas.map(idioma => idioma.nm_idioma);
+        const  ListSearchLanguage  =  list_idioma.map(idioma => idioma.nm_idioma);
+        const idioma = await connection('Idioma').select('nm_idioma').whereIn('nm_idioma', ListSearchLanguage);
 
         if(idioma.length > 0){
         return res.status(400).json({ msg: 'Um ou mais cursos já existem na base de dados' });
         }
 
-        const idiomaList = list_linguas.map(idioma => ({ nm_idioma: idioma.nm_idioma, id_tipo_idioma: idioma.id_idioma }));
+        const idiomaList = list_idioma.map(idioma => ({ nm_idioma: idioma.nm_idioma }));
         const newLanguage = await connection('Idioma').insert(idiomaList);
 
         if (newLanguage == undefined || newLanguage.length == 0) {
@@ -260,7 +260,7 @@ module.exports = {
         for (let i = 0; i < idiomaList.length; i++) {
             const idiomaN = await connection('Idioma')
             .select("*").where('nm_idioma', idiomaList[i].nm_idioma)
-            .andWhere('Idioma.id_idioma', idiomaList[i].id_idioma);
+;
             data[idiomaN[0].nm_idioma] = idiomaN[0];
         }
         console.log(data);
