c6224ab58d70019f200fd75f33b68ef5247aebe4
CodeWracker <dev.rodrigofs@gmail.com>
2022-12-09 19:22:17 -0300

    insomnia

--- src/routes.js
+++ src/routes.js
@@ -43,6 +43,9 @@ routes.get('/info/postos', InfoController.PostoController.getPostos);
 
 
 routes.get('/info/tipo_restricao', InfoController.RestricaoController.getTipoRestricao);
+// routes.put('/info/tipo_restricao/:id_tipo_restricao', InfoController.RestricaoController.updateTipoRestricao);
+// routes.post('/info/tipo_restricao', InfoController.RestricaoController.createTipoRestricao);
+// routes.delete('/info/tipo_restricao/:id_tipo_restricao', InfoController.RestricaoController.deleteTipoRestricao);
 
 routes.get('/info/lotacao', InfoController.LotacaoController.getAllLotacao)
 routes.post('/info/lotacao', InfoController.LotacaoController.createLotacao);
